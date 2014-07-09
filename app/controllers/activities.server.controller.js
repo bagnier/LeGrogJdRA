3b5793951e2c86a3c4af93e46f270c319b49bf0d
Stéphane Bagnier <stephane.bagnier@gmail.com>
2014-07-09 08:13:44 +0200

    activity controller populates the article title

--- app/controllers/activities.server.controller.js
+++ app/controllers/activities.server.controller.js
@@ -95,7 +95,7 @@ exports.delete = function(req, res) {
 /**
  * List of Activities
  */
-exports.list = function(req, res) { Activity.find().sort('-created').populate('user', 'displayName').exec(function(err, activities) {
+exports.list = function(req, res) { Activity.find().sort('-created').populate('user', 'displayName').populate('article', 'title').exec(function(err, activities) {
 		if (err) {
 			return res.send(400, {
 				message: getErrorMessage(err)
@@ -109,7 +109,7 @@ exports.list = function(req, res) { Activity.find().sort('-created').populate('u
 /**
  * Activity middleware
  */
-exports.activityByID = function(req, res, next, id) { Activity.findById(id).populate('user', 'displayName').exec(function(err, activity) {
+exports.activityByID = function(req, res, next, id) { Activity.findById(id).populate('user', 'displayName').populate('article', 'title').exec(function(err, activity) {
 		if (err) return next(err);
 		if (! activity) return next(new Error('Failed to load Activity ' + id));
 		req.activity = activity ;
